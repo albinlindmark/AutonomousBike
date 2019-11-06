40b70f443e1fbf0837e068723044bcc2f9377333
JakobWadman <jakob.wadman@gmail.com>
2019-11-06 15:38:44 +0100

    Change of reset function

--- Environments/BikeLQR.py
+++ Environments/BikeLQR.py
@@ -21,12 +21,12 @@ class BikeLQREnv(gym.Env):
 
     def __init__(self):
         self.state = None
+        self.reward = None
         self.Ts = 0.04
         self.A = np.array([[1.015144907891091, 0.070671622176451], [0.431844962338814, 1.015144907891091]], dtype=np.float32)
         self.B = np.array([[0.001757594373457], [0.000476400936242]], dtype=np.float32)
         self.Q = np.array([[10, 0], [0, 0]], dtype=np.float32)
         self.R = 1
-        self.reward = 0
 
 
     def step(self, action):
@@ -39,7 +39,8 @@ class BikeLQREnv(gym.Env):
         # I.e. phi = uniform(-15, 15), dphi = 0 and delta = 0.
         # Note that x1 = phi and x2 =  b*h*dphi - a*v*delta.
         phi_0 = np.pi/180 * np.random.uniform(-15, 15)
-        self.state = np.array([[phi_0], [0]], dtype=np.float32) 
+        self.state = np.array([[phi_0], [0]], dtype=np.float32)
+        self.reward = 0
         
     def render(self, mode='human'):
         print('hej')
