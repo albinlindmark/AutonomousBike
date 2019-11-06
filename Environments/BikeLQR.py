110b5888a322057c9d495844947324748e124a46
JakobWadman <jakob.wadman@gmail.com>
2019-11-06 15:33:55 +0100

    Change of reset function

--- Environments/BikeLQR.py
+++ Environments/BikeLQR.py
@@ -33,12 +33,13 @@ class BikeLQREnv(gym.Env):
         self.reward += -(self.state.transpose() @ self.Q @ self.state + self.R*action**2)
         self.state = self.A @ self.state + self.B @ action
         
-        
-        
-        
 
     def reset(self):
-        1
+        # Start with random roll angle, zero roll angle rate and  zero steering angle.
+        # I.e. phi = uniform(-15, 15), dphi = 0 and delta = 0.
+        # Note that x1 = phi and x2 =  b*h*dphi - a*v*delta.
+        phi_0 = np.pi/180 * np.random.uniform(-15, 15)
+        self.state = np.array([[phi_0], [0]], dtype=np.float32) 
         
     def render(self, mode='human'):
         print('hej')
