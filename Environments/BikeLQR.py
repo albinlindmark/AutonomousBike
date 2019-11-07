83047c68d714527c1e94fc830e4ef6e206f6f72c
JakobWadman <jakob.wadman@gmail.com>
2019-11-07 14:02:42 +0100

    Fix of reward function and addition of file to test the environment

--- Environments/BikeLQR.py
+++ Environments/BikeLQR.py
@@ -22,28 +22,31 @@ class BikeLQREnv(gym.Env):
     def __init__(self):
         self.state = None
         self.reward = None
-        # The ss matrices were created with Ts = 0.04 s and v = 5 m/s
+        
+        # The state space matrices were created with Ts = 0.04 s and v = 5 m/s
         self.A = np.array([[1.015144907891091, 0.070671622176451], [0.431844962338814, 1.015144907891091]], dtype=np.float32)
         self.B = np.array([[0.210654076615514], [1.042632885238932]], dtype=np.float32)
+        
         self.Q = np.array([[10, 0], [0, 0]], dtype=np.float32)
         self.R = 1
 
 
     def step(self, action):
-        self.reward += -(self.state.transpose() @ self.Q @ self.state + self.R*action**2)
+        self.reward = -(self.state.transpose() @ self.Q @ self.state + self.R*action**2).item()
         self.state = self.A @ self.state + self.B @ action
         # If roll angle larger than 30 degrees, then terminate:
         if self.state[0] > 30*np.pi/180:
             self.done = True
         
-        return self.state, self.reward, self.done, _
+        return self.state, self.reward, self.done, {}
         
 
     def reset(self):
         # Start with random roll angle, zero roll angle rate and  zero steering angle.
-        # I.e. phi = uniform(-15, 15), dphi = 0 and delta = 0.
+        # I.e. phi(0) = uniform(-15, 15), dphi(0) = 0 and delta(0) = 0.
         # Note that x1 = phi and x2 =  b*h*dphi - a*v*delta.
         phi_0 = np.pi/180 * np.random.uniform(-15, 15)
+        #phi_0 = np.pi/180*15 # to test with constant initial condition
         self.state = np.array([[phi_0], [0]], dtype=np.float32)
         self.reward = 0
         self.done = False
---
+++ Environments/test_bike_env.py
@@ -0,0 +1,33 @@
+''' Simple script to test if the BikeLQR environment seems to work as intended.
+The LQR gain K for the speed v = 5 m/s (the same speed used for to calculate the state space
+matrices in the BikeLQR environment) is gotten from the matlab script LQR_measure_all_no_kalman.
+So this python script uses the -K*x as action to the environment and then plots
+how phi changes with this action. Then you can compare this plot to the simulation
+plot in matlab/simulink. The cumulative reward is also printed for each time step,
+which can be compared to the total cost in the matlab/simulink simulation. '''
+
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+K = np.array([[2.153841222845478, 0.298295153292258]], dtype=np.float32)
+Ts = 0.04
+nr_time_steps = 50
+
+env = gym.make("BikeLQR-v0")
+state = env.reset()
+cumulative_reward = 0
+phi_list = [state[0, 0]*180/np.pi]
+for i in range(nr_time_steps):
+    state, reward, done, _ = env.step(-K@state)
+    phi_list.append(state[0].item()*180/np.pi)
+    
+    if done:
+        break
+    cumulative_reward += reward
+    print('Cumulative reward', cumulative_reward)
+
+
+t = np.linspace(0, len(phi_list)*Ts, len(phi_list))
+fig, axes = plt.subplots(figsize=(14,8))
+axes.plot(t, phi_list)
