00511e4382089fc7c50c29900d58747cc6cccd55
JakobWadman <jakob.wadman@gmail.com>
2019-11-10 12:39:52 +0100

    Minor updates on test_bike_env

--- Environments/test_bike_env.py
+++ Environments/test_bike_env.py
@@ -10,22 +10,26 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
-K = np.array([[2.153841222845478, 0.298295153292258]], dtype=np.float32)
+K = np.array([2.153841222845478, 0.298295153292258], dtype=np.float32)
 Ts = 0.04
 nr_time_steps = 50
 
 env = gym.make("BikeLQR-v0")
 state = env.reset()
+print('state_0', state)
 cumulative_reward = 0
-phi_list = [state[0, 0]*180/np.pi]
+phi_list = [state[0]*180/np.pi]
 for i in range(nr_time_steps):
-    state, reward, done, _ = env.step(-K@state)
+    state, reward, done, _ = env.step([-K@state])
+    #state, reward, done, _ = env.step(np.array([0], dtype=np.float32))
     phi_list.append(state[0].item()*180/np.pi)
+    
+    cumulative_reward += reward
+    print('Cumulative reward', cumulative_reward)
+    print('Done:', done)
     
     if done:
         break
-    cumulative_reward += reward
-    print('Cumulative reward', cumulative_reward)
 
 
 t = np.linspace(0, len(phi_list)*Ts, len(phi_list))
