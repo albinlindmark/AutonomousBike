00acbbe21e98678d6c9e8e585f5a11b6c19f7123
JakobWadman <jakob.wadman@gmail.com>
2019-11-10 12:38:59 +0100

    Environment without normalization

--- Environments/BikeLQR.py
+++ Environments/BikeLQR.py
@@ -23,28 +23,33 @@ class BikeLQREnv(gym.Env):
     def __init__(self):
         self.state = None
         self.reward = None
-        self.observation_space = spaces.Box(-np.inf, np.inf, shape=(2,), dtype=np.float32)
-        self.action_space = spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float32)
+        high = np.array([np.pi/2, np.finfo(np.float32).max])
+        self.observation_space = spaces.Box(low=-high, high=high, dtype=np.float32)
+        self.action_space = spaces.Box(-np.pi/2, np.pi/2, shape=(1,), dtype=np.float32)
         
         # The state space matrices were created with Ts = 0.04 s and v = 5 m/s
         self.A = np.array([[1.015144907891091, 0.070671622176451], [0.431844962338814, 1.015144907891091]], dtype=np.float32)
-        self.B = np.array([[0.210654076615514], [1.042632885238932]], dtype=np.float32)
+        self.B = np.array([0.210654076615514, 1.042632885238932], dtype=np.float32)
         
-        self.Q = np.array([[10, 0], [0, 0]], dtype=np.float32)
-        self.R = np.array(1, dtype=np.float32)
-        # P was calculated using DARE with the A, B, Q and R matrices
-        self.P = np.array([[20.010202549140178, 0.776071023429386], [0.776071023429386,0.195896896613182]], dtype=np.float32)
+        self.Q = np.array([[10, 0], [0, 0]])
+        self.R = 1
 
 
-    def step(self, action):            
-        cost = (self.state.transpose() @ self.Q @ self.state + self.R*action**2).item()
-        optimal_final_cost = self.state_0.transpose() @ self.P @ self.state_0
-        self.reward = -cost/optimal_final_cost # the costs get normalized with optimal final cost
+    def step(self, action):
+        # Make sure that the action (delta) is in interval [-pi/2, pi/2]:
+        action = np.clip(action, -np.pi/2, np.pi/2)[0]
+        cost = (self.state.transpose() @ self.Q @ self.state + action**2*self.R)
+        self.reward = -cost
         
-        self.state = self.A @ self.state + self.B @ action
+        self.state = self.A @ self.state + self.B * action # action is scalar
         # If roll angle larger than 30 degrees, then terminate:
-        if abs(self.state[0] > 30*np.pi/180):
+        if abs(self.state[0]) > 30*np.pi/180:
             self.done = True
+            worst_case_phi = np.pi/180*15
+            worst_case_state = np.array([worst_case_phi, 0], dtype=np.float32)
+            worst_case_action = np.pi/2
+            cost = (worst_case_state.transpose() @ self.Q @ worst_case_state + worst_case_action**2*self.R)
+            self.reward = -100 * cost
         
         return self.state, self.reward, self.done, {}
         
@@ -53,10 +58,10 @@ class BikeLQREnv(gym.Env):
         # Start with random roll angle, zero roll angle rate and  zero steering angle.
         # I.e. phi(0) = uniform(-15, 15), dphi(0) = 0 and delta(0) = 0.
         # Note that x1 = phi and x2 =  b*h*dphi - a*v*delta.
-        phi_0 = np.pi/180 * np.random.uniform(-15, 15)
-        #phi_0 = np.pi/180*15 # to test with constant initial condition
+        #phi_0 = np.pi/180 * np.random.choice([np.random.uniform(-15, -1), np.random.uniform(1, 15)]) 
+        phi_0 = np.pi/180*(5 + np.random.uniform(-0.1, 0.1)) 
+        #phi_0 = np.pi/180*15
         self.state = np.array([phi_0, 0], dtype=np.float32)
-        self.state_0 = self.state.copy()
         self.reward = 0
         self.done = False
         return self.state
